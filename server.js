ae0989f040f95a4a99e3fa6ce7e44e329908bb74
Olorunfemi Samson <samsie012@gmail.com>
2022-06-26 21:40:43 +0100

    Version1

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000
+
+app.set("view engine", 'ejs')
+
+app.get('/',(req,res)=>{
+    res.status(200).render("base", {title: "Login system"});
+
+})
+
+app.listen(port,console.log("Listening to server on http://localhost:3000"));
